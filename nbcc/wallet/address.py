d5361e3a706ced6c722fd3f4c757869119297320
fn_share <fn_share@126.com>
2026-03-09 14:14:34 +0800

    26-03-09

--- nbcc/wallet/address.py
+++ nbcc/wallet/address.py
@@ -56,7 +56,6 @@ def _aesDecrypt(sText, passphrase=''):
   return sDecoded
 
 def _keyFromPoint(point, compressed):
-  'Converts a point into a key.'
   key = ( b'\x04' +
           number_to_string(point.x(),curve.order) +
           number_to_string(point.y(),curve.order) )
@@ -70,6 +69,35 @@ def _sigencode(r, s, order):
 def _sigdecode(s, order):
   return (int(hexlify(s[:32]),16),int(hexlify(s[32:]),16))
 
+# Class Address descript secp256k1 curve address, it is used for
+# blockchain account.
+#
+# The account usually take a public key and a private key, however
+# private key can be abscent, the account supports verify signature
+# but no making signature if private key abscent, nevertheless,
+# if private key is included, the public key can be calculated
+# from private key, and the account both support making signature
+# and verify signature.
+# 
+# Args:
+#   pub_key (None or bytes): None means public key is abscent (it 
+#     will depend to `priv_key`), pass 33-bytes means it is
+#     compressed public key and its first byte should be b'\x02'
+#     or b'\x03', pass 65-bytes means it is uncompressed public
+#     key and its first byte should be b'\x04'
+#   priv_key (None or bytes): None for private key abscent, 
+#     otherwize it should be WIF format string, argument pub_key 
+#     and priv_key can not both be None
+#   ver (bytes): version of account, it usually take b'\x00' 
+#     that means version 0
+# 
+# Examples:
+#   >>> Address(public_key_33_bytes)
+#   <nbcc.wallet.address.Address object at 0x105205940>
+#   >>> from nbcc import util
+#   >>> priv = util.key.privkey_to_wif(private_key_32_bytes)
+#   >>> Address(priv_key=priv)
+#   <nbcc.wallet.address.Address object at 0x10543da90>
 class Address(object):
   def __init__(self, pub_key=None, priv_key=None, ver=b'\x00'):
     self._compressed = False
@@ -119,35 +147,56 @@ class Address(object):
     # public address, according to uncompressed
     self._address = util.key.publickey_to_address(self._pub_key,self._ver)
   
+  # Get base58 encoded account address
+  # Returns:
+  #   human readable address string that encoded by base58
   def address(self):
     return self._address
   
-  def publicHash(self):   # according to compressed
+  # Get public key hash by ripemd160(sha256d(public_key_33_bytes))
+  # Returns:
+  #   hash string in bytes
+  def publicHash(self):
     return util.key.publickey_to_hash(self.publicKey())
   
-  def publicKey(self):    # according to compressed
+  # Get compressed public key
+  # Returns:
+  #   public key in bytes
+  def publicKey(self):
     if self._comp_pubkey is None:
       self._comp_pubkey = util.key.compress_public_key(self._pub_key)
     return self._comp_pubkey
   
+  # Get private key
+  # Returns:
+  #   private key in bytes
   def _priv_key_(self):
     if self._priv_key is None:
       return None
     return util.key.privkey_from_wif(self._priv_key)  # the binary representation of private
   
+  # Get fingerprint of public key
+  # Returns:
+  #   4 bytes fingerprint, by ripemd160(sha256(public_key33))[:4]
   def fingerprint(self):
     return hash160(self.publicKey())[:4]
   
+  # Generate new random Address account
+  # Args:
+  #   ver (bytes): version of Address
+  # Returns:
+  #   4 bytes fingerprint, by ripemd160(sha256(public_key33))[:4]
   @staticmethod
-  def generate(ver=b'\x00', compressed=True):   # suggest only using compressed address
-    'Generate a new random address.'
+  def generate(ver=b'\x00'):
     secexp = randrange(curve.order)             # return: 1 <= k < order
     key = number_to_string(secexp,curve.order)  # get 32 bytes number
-    if compressed:
-      key = key + b'\x01'
+    key += b'\x01'       # with compressed
     return Address(priv_key=util.key.privkey_to_wif(key),ver=ver)
   
-  def decompress(self):  # convert to decompressed
+  # Convert to public-key-uncompressed Address account
+  # Returns:
+  #   Address instance
+  def decompress(self):
     if not self._compressed: return self
     
     if self._priv_key:
@@ -156,7 +205,10 @@ class Address(object):
       return Address(pub_key=util.key.decompress_public_key(self._pub_key),ver=self._ver)
     raise ValueError('address cannot be decompressed')
   
-  def compress(self):    # convert to compress
+  # Convert to public-key-compressed Address account
+  # Returns:
+  #   Address instance
+  def compress(self):
     if self._compressed: return self
     
     if self._priv_key:
@@ -165,10 +217,22 @@ class Address(object):
       return Address(pub_key=self.publicKey(),ver=self._ver)
     raise ValueError('address cannot be compressed')
   
+  # Get 32 bytes private key
+  # Returns:
+  #   private key in bytes
   def _get_priv(self):
     if self._priv_key is None: raise ValueError('invalid private key')
     return util.key.privkey_from_wif(self._priv_key)
   
+  # Make signature
+  # Args:
+  #   data (bytes): the string to be sign
+  #   single (bool): apply hash256() on `data` one time or twice
+  #     True is one time, False is double time
+  #   no_der (bool): the result signature in DER format or not,
+  #     True for no DER format, False for DER format
+  # Returns:
+  #   signature in bytes
   def sign_ex(self, data, single=False, no_der=False):
     if self._sign_key is None:
       self._sign_key = SigningKey.from_string(self._get_priv(),curve)
@@ -177,6 +241,17 @@ class Address(object):
     sig_encode = _sigencode if no_der else sigencode_der
     return self._sign_key.sign_digest(ha,sigencode=sig_encode)
   
+  # Verify signature
+  # Args:
+  #   data (bytes): the input string to be verified
+  #   signature (bytes): the signature of `data`
+  #   single (bool): apply hash256() on `data` one time or twice
+  #     True is one time, False is double time
+  #   no_der (bool): `signature` in DER format or not,
+  #     True for no DER format, False for DER format
+  # Returns:
+  #   return bytes value, if return b'' means verify failed,
+  #   otherwise return 32 bytes of hash string means verify OK
   def verify_ex(self, data, signature, single=False, no_der=False):
     if self._verify_key is None:
       if self._priv_key:
@@ -198,16 +273,42 @@ class Address(object):
       traceback.print_exc()
     return b''
   
-  def sign(self, data):  # signs data with private key
+  # Make signature by doulbe hash256 on `data` and result in DER format
+  # Args:
+  #   data (bytes): the string to be sign
+  # Returns:
+  #   signature in bytes
+  def sign(self, data):
     return self.sign_ex(data)
   
-  def verify(self, data, signature):  # verifies data and signature with public key
+  # Verify signature, the `signature` come from double hash256()
+  #   on `data` and it is in DER format
+  # Args:
+  #   data (bytes): the input string to be verified
+  #   signature (bytes): the signature of `data`
+  # Returns:
+  #   return bool value, True for verify passed, and False for 
+  #   failed (not pass)
+  def verify(self, data, signature):
     return self.verify_ex(data,signature) != b''
   
-  def sign_noder(self, data):  # sign without DER
+  # Make signature by doulbe hash256 on `data` and without DER format in result
+  # Args:
+  #   data (bytes): the string to be sign
+  # Returns:
+  #   signature in bytes
+  def sign_noder(self, data):
     return self.sign_ex(data,no_der=True)
   
-  def verify_noder(self, data, signature):  # verify without DER
+  # Verify signature, the `signature` come from double hash256()
+  #   on `data` and it is not in DER format
+  # Args:
+  #   data (bytes): the input string to be verified
+  #   signature (bytes): the signature of `data`
+  # Returns:
+  #   return bytes value, if return b'' means verify failed,
+  #   otherwise return 32 bytes of hash string means verify OK
+  def verify_noder(self, data, signature):
     return self.verify_ex(data,signature,no_der=True)
   
   def __str__(self):
@@ -215,6 +316,13 @@ class Address(object):
     if self._priv_key: privateKey = '**redacted**'
     return '<Address address=%s private=%s>' % (self._address,privateKey)
   
+  # Dump account to dict result
+  # Args:
+  #   passphrase (str or bytes): for encrypting private key
+  #   cfg (None or dict): pass None for creating new dict to hold
+  #     result, also can pass dict value to hold dumping result
+  # Returns:
+  #   dict type result that hold dumping result
   def dump_to_cfg(self, passphrase='', cfg=None):
     cfg = cfg or {}
     account = { 'time':int(time.time()), 'encrypted':False, 'type':'default',
@@ -240,6 +348,12 @@ class Address(object):
     
     return cfg
   
+  # Load account from properties that hold in dict
+  # Args:
+  #   account (dict): hold all properties for account
+  #   passphrase (str or bytes): for decrypting private key
+  # Returns:
+  #   return Address instance when successful
   @staticmethod
   def load_from_cfg(account, passphrase=''):
     pubKey = account['pubkey']; prvKey = account['prvkey']
